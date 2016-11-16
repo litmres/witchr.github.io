9dbfe89e7d0460f630e6e4df35d48ec2b12f3a72
afable <afablee@gmail.com>
2016-11-16 13:37:03 -0800

    reduced geometry to a single point (square) that appears on the screen

--- js/witchr.js
+++ js/witchr.js
@@ -386,16 +386,9 @@
 		// Base class for geometries.
 		// A geometry holds all data necessary to describe a 3D model.
 		geometry = new THREE.Geometry(); // basic geometry is a square
-		for ( let i = 0; i < 100000; ++i ) {
-			let vertex = new THREE.Vector3();
-			vertex.x = Math.random() * 2000 - 1000;
-			vertex.y = Math.random() * 2000 - 1000;
-			vertex.z = Math.random() * 2000 - 1000;
-			// push the vertices of geometry x,y,z location
-			geometry.vertices.push( vertex );
-		}
+		geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
 		// size of each point (square) and special blending effects
-		material = new THREE.PointsMaterial( { size: 100, blending: THREE.AdditiveBlending, depthTest: false, transparent: true } );
+		material = new THREE.PointsMaterial( { size: 10, blending: THREE.AdditiveBlending, depthTest: false, transparent: true } );
 		material.color.setHSL( 0, 0.9, 0.5 ); // h, s, l values between 0.0 and 1.0 
 		// add all vertex points to THREE.Points class (like THREE.Mesh)
 		let particle = new THREE.Points( geometry, material );
